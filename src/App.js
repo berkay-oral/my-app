0329278dc5e2ddb12f1ccb9730ed27b87f5a9dde
codeberkay <50360041+codeberkay@users.noreply.github.com>
2021-06-03 01:30:49 +0300

    message

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import myImage from "../src/components/images/my-photo.jpg";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+                <h1>Burası anasayfam</h1>
+                <img src={myImage} />
+    </div>
+  );
+}
+
+export default App;
